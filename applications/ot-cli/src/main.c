cbda5f1669dddc237bada3302fa507656e768330
chengruei.wei <chengruei.wei@realtek.com>
2024-07-04 12:46:01 +0800

    Add ot-cli project
    Signed-off-by: chengruei.wei <chengruei.wei@realtek.com>

---
+++ applications/ot-cli/src/main.c
@@ -0,0 +1,50 @@
+#include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+#include <version.h>
+#include <zephyr/logging/log.h>
+#include <stdlib.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+#include <ctype.h>
+
+#ifdef CONFIG_ARCH_POSIX
+#include <unistd.h>
+#else
+#include <zephyr/posix/unistd.h>
+#endif
+
+LOG_MODULE_REGISTER(app);
+
+#include <openthread/instance.h>
+#include <openthread/platform/time.h>
+#include <openthread/platform/alarm-milli.h>
+#include <openthread/platform/alarm-micro.h>
+#include <openthread-system.h>
+#include <trace.h>
+
+// replace misc.c
+extern void WDG_SystemReset(int wdt_mode, int reset_reason);
+void __wrap_otPlatReset(otInstance *aInstance)
+{
+ 	ARG_UNUSED(aInstance);
+	WDG_SystemReset(0, 0xff);
+}
+
+int main(void)
+{
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	const struct device *dev;
+	uint32_t dtr = 0;
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (!device_is_ready(dev) || usb_enable(NULL)) {
+		return 0;
+	}
+
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+#endif
+	return 0;
+}
