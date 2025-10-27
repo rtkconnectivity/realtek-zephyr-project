7dc427204072ec49eb074cf9fba989a7d6357c3f
Zophai Liu <zophai_liu@realsil.com.cn>
2025-10-27 16:38:40 +0800

    Tool: fix zephyr env dependence
    Fix unreasonable env dependence
    
    Signed-off-by: Zophai Liu <zophai_liu@realsil.com.cn>

--- tool/tool.py
+++ tool/tool.py
@@ -6,31 +6,29 @@ import platform
 import subprocess
 import sys
 from typing import Any, Dict, List, Type
-import yaml
 from pathlib import Path
 
+THIS_ZEPHYR = Path(__file__).parents[2] / 'zephyr'
+ZEPHYR_BASE = Path(os.environ.get('ZEPHYR_BASE', THIS_ZEPHYR))
+
+sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts", "west_commands"))
 from textwrap import dedent           
 from west.commands import WestCommand 
 from west.configuration import config
 from west import log
 
-THIS_ZEPHYR = Path(__file__).parents[4] / 'zephyr'
-ZEPHYR_BASE = Path(os.environ.get('ZEPHYR_BASE', THIS_ZEPHYR))
-
-sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts", "west_commands"))
-
 from build_helpers import find_build_dir, is_zephyr_build, \
     FIND_BUILD_DIR_DESCRIPTION
 from runners.core import BuildConfiguration
-from zcmake import CMakeCache
-from zephyr_ext_common import Forceable, ZEPHYR_SCRIPTS
+
 log.set_verbosity(log.VERBOSE_NONE)
 
 def cmd_exec(cmd, cwd=None, shell=False):
+    """Execute a command and return its output"""
     log.dbg(str(cmd))
     return subprocess.check_call(cmd, cwd=cwd, shell=shell)
 
-class RealtekBee(Forceable):
+class RealtekBee(WestCommand):
 
     def __init__(self):
         super().__init__(
@@ -39,9 +37,18 @@ class RealtekBee(Forceable):
             dedent('''
             Realtek tools for west framework'''))
         self.tool_classes: List[Type[Tool]] = [PrependHeader, MD5, MPCLI, PACKCLI]
+ 
+    def check(self, cond, msg, exit) -> bool:
+        if not cond:
+            self.wrn(msg)
+            if exit:
+                sys.exit(1)
+            return False
+        else:
+            return True
 
     def do_add_parser(self, parser_adder):
-
+        """Add parser for Realtek Bee command"""
         parser = parser_adder.add_parser(self.name,
                                          help=self.help,
                                          description=self.description)
@@ -53,15 +60,17 @@ class RealtekBee(Forceable):
         return parser
 
     def do_run(self, args, unknown_args):
+        self.args = args
+
         # Find the build directory and parse .config and DT.
         build_dir = find_build_dir(args.build_dir)
-        self.check_force(os.path.isdir(build_dir),
-                         'no such build directory {}'.format(build_dir))
-        self.check_force(is_zephyr_build(build_dir),
-                         "build directory {} doesn't look like a Zephyr build "
-                         'directory'.format(build_dir))
-        build_conf = BuildConfiguration(build_dir)
-        
+        self.check(os.path.isdir(build_dir),
+                    'no such build directory {}'.format(build_dir), True)
+        if not self.check(is_zephyr_build(build_dir),
+                "build directory {} doesn't look like a Zephyr build "
+                'directory'.format(build_dir), False):
+            build_dir = None
+
         if unknown_args:
             log.inf(f"unknown_args:{unknown_args}")
         # ToDo: get -b from IC type
@@ -74,69 +83,55 @@ class RealtekBee(Forceable):
         ic_series = "Bee"
         tools_path = Path(module_path, f"tool/bin/{ic_series}")
 
-        # if args.sdk_dir:
-        #     rtk_sdk_path = Path(args.sdk_dir)
-        # else:
-        #     rtk_sdk_path = Path(os.environ.get('RTK_SDK_DIR', ""))
-        # log.inf(f"rtk_sdk_path:{rtk_sdk_path}")
-        # if rtk_sdk_path:
-        #     tool_bin_path = Path(rtk_sdk_path, "tool/")
-        
         for tool_class in self.tool_classes:
             if args.subcommand == tool_class.name:
                 print(f"Start {tool_class.name}")
-                tool_instance = tool_class(tools_path, build_dir, build_conf)
+                tool_instance = tool_class(tools_path, build_dir)
                 tool_instance.do_run(args, unknown_args)
 
 class Tool(abc.ABC):
-    '''Common abstract superclass for tool.
-
-    To add support for a new tool, subclass this and add support for
-    it in the Sign.do_run() method.'''
+    """Common abstract superclass for tools"""
     name = ""
-    def __init__(self, tools_path, build_dir, build_conf: BuildConfiguration):
-        self.tools_path = tools_path  
-        self.build_dir = Path(build_dir)
-        self.build_conf = build_conf
-        # load properties
-        self.kernel = build_conf.get('CONFIG_KERNEL_BIN_NAME', 'zephyr')
-        self.soc_series = build_conf.get('CONFIG_SOC_SERIES', 'none')
-        self.in_bin = self.build_dir / 'zephyr' / f'{self.kernel}.bin'
-    
+    def __init__(self, tools_path, build_dir=None):
+        self.tools_path = tools_path
+        if build_dir:
+            self.build_dir = Path(build_dir)
+            self.build_conf = BuildConfiguration(build_dir)
+
+            # load properties
+            self.kernel = self.build_conf.get('CONFIG_KERNEL_BIN_NAME', 'zephyr')
+            self.soc_series = self.build_conf.get('CONFIG_SOC_SERIES', 'none')
+            self.in_bin = self.build_dir / 'zephyr' / f'{self.kernel}.bin'
+        else:
+            self.build_conf = None
+
     @classmethod
     @abc.abstractmethod
     def do_add_parser(self, parser):
-        '''
-        :param parser
-        '''
+        """Add parser for the tool"""
 
     @abc.abstractmethod
     def do_run(self, args, unknown_args):
-        '''
-        '''
-    
-    @staticmethod
-    def flash_address_from_build_conf(build_conf: BuildConfiguration):
-        '''If CONFIG_HAS_FLASH_LOAD_OFFSET is n in build_conf,
-        return the CONFIG_FLASH_BASE_ADDRESS value. Otherwise, return
-        CONFIG_FLASH_BASE_ADDRESS + CONFIG_FLASH_LOAD_OFFSET.
-        '''
-        if build_conf.getboolean('CONFIG_HAS_FLASH_LOAD_OFFSET'):
-            return (build_conf['CONFIG_FLASH_BASE_ADDRESS'] +
-                    build_conf['CONFIG_FLASH_LOAD_OFFSET'])
-        else:
-            return build_conf['CONFIG_FLASH_BASE_ADDRESS']
-    
+        """Run the tool"""
+
+    def forceable_build_conf(self, msg=""):
+        """Check if build configuration is available"""
+        if not self.build_conf:
+            log.err("Zephyr build directory is required ", msg)
+            sys.exit(1)
+
 class PrependHeader(Tool):
-    name = 'prepend'             
+    name = 'prepend'
+             
     @classmethod
     def do_add_parser(cls, parser):
         prepend_parser = parser.add_parser(cls.name, help='prepend header')
         prepend_parser.add_argument('-b', '--ictype', default='16', help='ic type')
-        
         return parser
+
     def do_run(self, args, unknown_args):
-        
+        self.forceable_build_conf()
+
         ini_path = Path(self.tools_path, "prepend_header/mp.ini")
         cmd_path = Path(os.getcwd())
         if platform.system() == 'Windows':
@@ -157,30 +152,38 @@ class PrependHeader(Tool):
 
 class MD5(Tool):
     name = 'md5'
+
     @classmethod
     def do_add_parser(cls, parser):
-        prepend_parser = parser.add_parser(cls.name, help='md5')
+        parser.add_parser(cls.name, help='md5')
         return parser
+
     def do_run(self, args, unknown_args):
+        # check params required
+        self.forceable_build_conf()
+
         in_bin_mp_path = self.in_bin.with_name(self.in_bin.stem + "_MP" + self.in_bin.suffix)
         cmd_path = Path(os.getcwd())
+
         if platform.system() == 'Windows':
             md5_path = Path(self.tools_path, "md5/md5.exe")
         elif platform.system() == 'Darwin':
             md5_path = Path(self.tools_path, "md5/MD5.mac")
         else:
-            log.err('not supportted')
+            log.err(f"{platform.system()} not supportted")
+
         cmd_exec((md5_path, in_bin_mp_path), cwd=cmd_path)
 
 class MPCLI(Tool):
     name = 'mpcli'
-    def __init__(self, tools_path, build_dir, build_conf):
-        super().__init__(tools_path, build_dir, build_conf)
+
+    def __init__(self, tools_path, build_dir):
+        super().__init__(tools_path, build_dir)
         self.port = ""
         self.baud = "1000000"
         self.relativepath = ""
         self.files: List[Dict[str, Any]] = []
-
+    
     @classmethod
     def do_add_parser(cls, parser):
         mpcli_parser = parser.add_parser(cls.name, formatter_class=argparse.RawTextHelpFormatter,help='MPCLI tool for firmware downloading',)
@@ -248,6 +251,18 @@ class MPCLI(Tool):
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(mptool_config, f, indent=4, ensure_ascii=False)
 
+    @staticmethod
+    def flash_address_from_build_conf(build_conf: BuildConfiguration):
+        '''If CONFIG_HAS_FLASH_LOAD_OFFSET is n in build_conf,
+        return the CONFIG_FLASH_BASE_ADDRESS value. Otherwise, return
+        CONFIG_FLASH_BASE_ADDRESS + CONFIG_FLASH_LOAD_OFFSET.
+        '''
+        if build_conf.getboolean('CONFIG_HAS_FLASH_LOAD_OFFSET'):
+            return (build_conf['CONFIG_FLASH_BASE_ADDRESS'] +
+                    build_conf['CONFIG_FLASH_LOAD_OFFSET'])
+        else:
+            return build_conf['CONFIG_FLASH_BASE_ADDRESS']
+
     def do_run(self, args, unknown_args):
         self.port = args.com_port
         cmd_path = Path(os.getcwd())
@@ -268,6 +283,7 @@ class MPCLI(Tool):
                 if not os.path.isfile(bin_file_path):
                     log.err('no such bin file {}'.format(bin_file_path))
             else:
+                self.forceable_build_conf("for downloading zephyr bin")
                 download_address = hex(self.flash_address_from_build_conf(self.build_conf))
                 bin_file_path = self.build_dir / 'zephyr'/ f'{self.kernel}.bin'
 
@@ -293,12 +309,12 @@ class MPCLI(Tool):
             cmd_exec(cmd_args, cwd=cmd_path)
         except Exception as e:
             print(e.args)
-            print(e)
 
 class PACKCLI(Tool):
     name = "packcli"
-    def __init__(self, tools_path, build_dir, build_conf):
-        super().__init__(tools_path, build_dir, build_conf)
+
+    def __init__(self, tools_path, build_dir, ):
+        super().__init__(tools_path, build_dir)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -347,4 +363,3 @@ class PACKCLI(Tool):
             cmd_exec(cmd_args, cwd=cmd_path)
         except Exception as e:
             print(e.args)
-            print(e)
